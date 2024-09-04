bfdf7da4c030559eff1d716583de759f63fb1f20
Zaytsev Aleksandr <145544514+ZaytsevAlexandr@users.noreply.github.com>
2024-09-04 14:47:21 +0300

    added

---
+++ lab_0/divmul.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+	int a = 14;
+	int b = 7;
+	int mul;
+	int div;
+	
+	mul = a * b;
+	div = a / b;
+
+	std::cout << mul << div << std::endl;
+
+	unsigned int ua = 21;
+	unsigned int ub = 3;
+	unsigned int umul;
+	unsigned int udiv;
+	
+	umul = ua * ub;
+	udiv = ua / ub;
+
+	std::cout << umul << udiv << std::endl;
+
+}
